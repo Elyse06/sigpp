be6a1564ed4e83e5f7f749b75ad50477b1242111
Elyse06 <144808199+Elyse06@users.noreply.github.com>
2023-10-05 09:49:59 +0300

    include css

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -7,6 +7,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>AdminLTE 3 | Starter</title>
 <link rel="stylesheet" href="public/css/app.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini">
