4130f9349cc22caccb80d540efb05022afccf1e1
Elyse06 <rabelaza4774@gmail.com>
2023-11-15 17:03:35 +0300

    filtrage

--- app/Http/Livewire/Etat.php
+++ app/Http/Livewire/Etat.php
@@ -8,17 +8,30 @@ use App\Models\Mission;
 use App\Models\Permission;
 use App\Models\RepoMedical;
 use App\Models\SortiePersonnel;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class Etat extends Component
 {
-    public function render()
+    public $dateDebut;
+    public $dateFin;
+
+	public function mount(Request $request)
+	{
+		$this->dateDebut = $request->input('dateDebut', now()->toDateString());
+		$this->dateFin = $request->input('dateFin', now()->toDateString());
+	}
+
+    public function render(Request $request)
     {
-        $mission = Mission::where('finmis', '>=', now()->toDateString())->where('debutmis', '<=', now()->toDateString())->get();
-    	$conge = Conge::where('fincon', '>=', now()->toDateString())->where('debutcon', '<=', now()->toDateString())->get();
-    	$permission = Permission::where('finpermi', '>=', now()->toDateString())->where('debutpermi', '<=', now()->toDateString())->get();
-    	$sortie = SortiePersonnel::where('finsortie', '>=', now()->toDateString())->where('debutsortie', '<=', now()->toDateString())->get();
-    	$repos = RepoMedical::where('finrep', '>=', now()->toDateString())->where('debutrep', '<=', now()->toDateString())->get();
+		$dateDebut = $request->input('dateDebut', $this->dateDebut);
+    	$dateFin = $request->input('dateFin', $this->dateFin);
+
+        $mission = Mission::where('finmis', '<=', $dateFin)->where('debutmis', '>=', $dateDebut)->get();
+    	$conge = Conge::where('fincon', '<=', $dateFin)->where('debutcon', '>=', $dateDebut)->get();
+    	$permission = Permission::where('finpermi', '<=', $dateFin)->where('debutpermi', '>=', $dateDebut)->get();
+    	$sortie = SortiePersonnel::where('finsortie', '<=', $dateFin)->where('debutsortie', '>=', $dateDebut)->get();
+    	$repos = RepoMedical::where('finrep', '<=', $dateFin)->where('debutrep', '>=', $dateDebut)->get();
         $total = Employee::get();
     	$mission_count = count($mission);    	
     	$conge_count = count($conge);
@@ -26,7 +39,7 @@ class Etat extends Component
     	$sortie_count = count($sortie);
     	$repos_count = count($repos);
     	$total_count = count($total);
-        return view('livewire.etat.index',compact('mission_count','conge_count','permission_count','sortie_count','repos_count','total_count'))
+        return view('livewire.etat.index',compact('mission_count','conge_count','permission_count','sortie_count','repos_count','total_count','dateDebut','dateFin'))
         ->extends("layouts.master")
         ->section("contenu");
     }
